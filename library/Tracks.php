f1ae1ba6cf0f11f1df2bbf24eae0dbcce6d9e98f
Doug Hurst <dalan.hurst@gmail.com>
2011-06-23 17:05:16 -0500

    Changed '/' literal to DIRECTORY_SEPARATOR constant

--- library/Tracks.php
+++ library/Tracks.php
@@ -1,25 +1,35 @@
 <?php
 /**
  * Tracks CQRS Framework
- * 
- * @author Sean Crystal <sean.crystal@gmail.com>
+ *
+ * PHP Version 5.3
+ *
+ * @category  Tracks
+ * @package   Tracks
+ * @author    Sean Crystal <sean.crystal@gmail.com>
  * @copyright 2011 Sean Crystal
- * @license http://www.opensource.org/licenses/BSD-3-Clause
- * @link https://github.com/spiralout/Tracks
+ * @license   http://www.opensource.org/licenses/BSD-3-Clause BSD 3-Clause
+ * @link      https://github.com/spiralout/Tracks
  */
 
 
 /**
  * Tracks library autoloader
- * 
- * @param string $className 
+ *
+ * @param string $className Class name
+ *
+ * @return null
  */
-function tracksAutoloader($className) {
-   $file = __DIR__ .'/'. str_replace('\\', '/', $className) .'.php';
-   
-   if (file_exists($file)) {
-      include $file;
-   }
+function tracksAutoloader($className)
+{
+    $file = __DIR__
+        .DIRECTORY_SEPARATOR
+        .str_replace('\\', DIRECTORY_SEPARATOR, $className)
+        .'.php';
+
+    if (file_exists($file)) {
+        include $file;
+    }
 }
 
 spl_autoload_register('tracksAutoloader');
